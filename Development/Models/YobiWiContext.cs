8c09bc515cf17f86cb324d19460c0a4b03ed82f0
NeytChi <neyton61@gmail.com>
2019-11-29 15:52:16 +0200

    Initiate ef context with configuration string from Config class.

--- Development/Models/YobiWiContext.cs
+++ Development/Models/YobiWiContext.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Common;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 
@@ -23,7 +24,7 @@ namespace YobiWi.Development.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseMySql("server=localhost;database=yobiWi;user=root;pwd=root;");
+                optionsBuilder.UseMySql(Config.GetDatabaseConfigConnection());
             }
         }
 
